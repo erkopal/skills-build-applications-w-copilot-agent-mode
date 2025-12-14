e7e31bfbdb5a5315d59cd0cc0963b5b7728fd29d
erkopal <er.kopal@gmail.com>
2025-12-14 06:59:03 +0000

    Update api_root in urls.py to use supreme-yodel-pq7j95p79jfrv46 for API URLs

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+
+import os
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -30,12 +32,17 @@ router.register(r'leaderboards', views.LeaderboardViewSet)
 
 @api_view(['GET'])
 def api_root(request, format=None):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f"https://{codespace_name}-8000.app.github.dev/api/"
+    else:
+        base_url = "http://localhost:8000/api/"
     return Response({
-        'users': request.build_absolute_uri('users/'),
-        'teams': request.build_absolute_uri('teams/'),
-        'activities': request.build_absolute_uri('activities/'),
-        'workouts': request.build_absolute_uri('workouts/'),
-        'leaderboards': request.build_absolute_uri('leaderboards/'),
+        'users': base_url + 'users/',
+        'teams': base_url + 'teams/',
+        'activities': base_url + 'activities/',
+        'workouts': base_url + 'workouts/',
+        'leaderboards': base_url + 'leaderboards/',
     })
 
 urlpatterns = [
